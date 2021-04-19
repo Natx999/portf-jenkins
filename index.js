490cf23a1b5a0b348ece7f98d039844b4087a3e4
Aran Shavit <aranshavit@gmail.com>
2021-04-19 21:18:02 +0300

    added random configuration

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-
 const log = require("json-log").log;
 const config = require('config')
 
+interval = config.interval
+count = config.count
+max = config.maxNum
+
 log.info("Starting rando app!");
+log.info(`Interval: ${interval}`)
+log.info(`Count: ${count}`)
+log.info(`MaxNum: ${max}`)
 
 async function main() {
   colors = ["red", "orange", "yellow", "green", "blue", "purple"]
   while (true) {
-    for (i = 0; i < config.count; i++) {
-      num = getRandomInt(config.maxNum)
-      color = colors[getRandomInt(6)]
-      log.info("Random data", { rando_details: { color: color , number: num}, time: new Date().toISOString()});
+    // Set values (defined or random)
+    if (interval != "random")
+    {
+      thisInterval = interval
     }
-    await sleep(config.interval*1000);
+    else {
+      thisInterval = getRandomInt(120)
+    }
+
+    if (count != "random")
+    {
+      thisCount = interval
+    }
+    else {
+      thisCount = getRandomInt(50)
+    }
+
+    // Write "thisCount" random log lines
+    for (i = 0; i < thisCount; i++) {
+      num = getRandomInt(max)
+      color = colors[getRandomInt(6)]
+      log.info("Random data", { rando_details: { color: color , number: num} });
+    }
+
+    // Sleep for "thisInterval" seconds
+    await sleep(thisInterval*1000);
+
   }
 }
 
